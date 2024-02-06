97cfb188885b503c41447a93f1e836cdf03c8831
Mohamad Taha <mhmd.m.taha@gmail.com>
2024-02-06 12:37:58 +0200

    Displaying Platform Icons

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,24 +1,34 @@
-import React from 'react'
-import { Game } from '../hooks/useGames'
-import { Card, CardBody, Heading, Image } from '@chakra-ui/react'
+import {
+  FaWindows,
+  FaPlaystation,
+  FaXbox,
+  FaApple,
+  FaLinux,
+  FaAndroid,
+} from "react-icons/fa";
+import {MdPhoneIphone} from 'react-icons/md'
+import {SiNintendo} from 'react-icons/si'
+import { Game } from "../hooks/useGames";
+import {BsGlobe} from 'react-icons/bs'
+import { Card, CardBody, Heading, Image, Text } from "@chakra-ui/react";
+import PlatformIconList from "./PlatformIconList";
 
-interface Props{
-    game: Game
+interface Props {
+  game: Game;
 }
 
-const GameCard = ({game}:Props) => {
+const GameCard = ({ game }: Props) => {
   return (
     <Card borderRadius="10px" overflow="hidden">
-        <Image src={game.background_image} />
-        <CardBody>
-            <Heading fontSize="2xl">
-                {game.name}
-            </Heading>
-        </CardBody>
-
-        
+      <Image src={game.background_image} />
+      <CardBody>
+        <Heading fontSize="2xl">{game.name}</Heading>
+        <PlatformIconList
+          platforms={game.parent_platforms.map((p) => p.platform)}
+        />
+      </CardBody>
     </Card>
-  )
-}
+  );
+};
 
-export default GameCard
+export default GameCard;
