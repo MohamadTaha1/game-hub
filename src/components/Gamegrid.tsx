f9797e115b6a5fa2cd0b1cb04d11f2d7d638cff0
Mohamad Taha <mhmd.m.taha@gmail.com>
2024-02-06 14:12:15 +0200

    Displaying the Genres

--- src/components/Gamegrid.tsx
+++ src/components/Gamegrid.tsx
@@ -4,17 +4,20 @@ import GameCard from "./GameCard";
 import GameCardSkeleton from "./GameCardSkeleton";
 import GameCardContainer from "./GameCardContainer";
 
-const skeletons = [1, 2, 3, 4, 5, 6];
+
 
 const Gamegrid = () => {
+const skeletons = [1, 2, 3, 4, 5, 6];
   const { data, error, isLoading } = useGames();
   return (
     <>
       {error && <Text>{error}</Text>}
       <SimpleGrid
         columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
-        spacing={10}
-        padding="10px"
+        padding = "10px"
+        spacing={3}
+        justifyContent={"space-evenly"}
+       
       >
         {isLoading &&
           skeletons.map((skeleton) => (
