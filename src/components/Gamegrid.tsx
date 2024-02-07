73504ffb6c7fffcffcf3cc12d9d4f8fce54c8aa4
Mohamad Taha <mhmd.m.taha@gmail.com>
2024-02-07 13:52:58 +0200

    Clean up Game cards

--- src/components/Gamegrid.tsx
+++ src/components/Gamegrid.tsx
@@ -18,9 +18,9 @@ const skeletons = [1, 2, 3, 4, 5, 6];
     <>
       {error && <Text>{error}</Text>}
       <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+        columns={{ sm: 1, md: 2, lg: 3, xl: 4}}
         padding = "10px"
-        spacing={3}
+        spacing={6}
         justifyContent={"space-evenly"}
        
       >
